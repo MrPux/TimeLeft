956ad53270ceacbad3115d820baa69c71a690577
MrPuxe <anthondeleonsketch@gmail.com>
2024-07-07 20:17:30 -0400

    Home, set up

---
+++ Home.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Home
+{
+	public static void main(String[] args){
+		// Gen Months
+		// gen Hours
+		// Get Beggining/Ending Date
+		// Get Beggining/Ending Time
+		// Calculate between time + return print results
+	}
+}
